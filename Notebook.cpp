c376623185004bc1a07e3372e965c58003c283c3
Omer Michael <74428768+omerMichael@users.noreply.github.com>
2022-03-22 18:23:22 +0200

    Create Notebook.cpp

---
+++ Notebook.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include "Direction.hpp"
+#include <iostream>
+#include "Notebook.hpp"
+
+using namespace std;
+using namespace ariel;
+
+void Notebook ::write(int page, int row, int col, Direction direction, std::string str){
+
+}
+
+string Notebook ::read(int page, int row, int col, Direction direction, int lenght){
+
+    return "";
+}
+
+void Notebook ::erase(int page, int row, int col, Direction direction, int lenght){
+
+}
+
+void Notebook ::show(int page){
+
+}
